f9441ab36a4a5980dd7dfcee7a27f8cea2e0dc55
thwbm <thwbm@qq.com>
2021-06-08 22:35:18 +0800

    二次初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Options, Vue } from 'vue-class-component'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
-export default defineComponent({
-  name: "Home",
+@Options({
   components: {
-    HelloWorld
-  }
-});
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
 </script>
